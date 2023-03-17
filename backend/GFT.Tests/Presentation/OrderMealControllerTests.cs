346d1e9f75fe8c97e771f759b7bd16f941888736
Felipe Carvalho <karvalho_182@hotmail.com>
2023-03-16 23:26:33 -0300

    chore: add tests configuration and Moq package

---
+++ backend/GFT.Tests/Presentation/OrderMealControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using GFT.Application.Protocols;
+using GFT.Application.Results;
+using GTF.Presentation.Controllers;
+using Moq;
+
+namespace GFT.Tests.Presentation;
+
+public class OrderMealControllerTests
+{
+    public class SutTypes
+    {
+        public OrderMealController OrderMealController { get; set; }
+        public OrderMealUseCaseStub OrderMealUseCaseStub { get; set; }
+    }
+
+    public class OrderMealUseCaseStub : IOrderMealUseCase
+    {
+        public UseCaseResult<string> Execute(string input)
+        {
+            return new UseCaseResult<string>("");
+        }
+    }
+
+    public static IOrderMealUseCase MakeUseCaseStub()
+    {
+        return new OrderMealUseCaseStub();
+    }
+
+    public SutTypes MakeSut()
+    {
+        var useCase = new OrderMealUseCaseStub();
+        var sut = new OrderMealController(useCase);
+
+        return new SutTypes
+        {
+            OrderMealController = sut,
+            OrderMealUseCaseStub = useCase
+        };
+    }
+}
